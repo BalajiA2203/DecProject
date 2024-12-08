fa58ebf655e8bd9d5d879883fa855d07f0aa1bc1
BalajiA2203 <Balajiarumugam2203@gmail.com>
2024-12-08 17:40:09 +0530

    latest update

--- src/test/java/GitClass.java
+++ src/test/java/GitClass.java
@@ -2,9 +2,12 @@ public class GitClass {
     public static void main(String[] args) {
         //git commands
         //git init
-        //git status
-        //git branch -m
+        //git status (always use to view all comments)
         //git add src/test/java
-        //git
+        //git commit -m
+        //git branch -m
+        //git remote add (repository name)
+        //git push -u newDescRepo javaDesc
+
     }
 }
