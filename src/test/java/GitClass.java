1f697a25ce2906e9646c992ceb8dc1e42f37eb64
BalajiA2203 <Balajiarumugam2203@gmail.com>
2024-12-08 17:25:13 +0530

    push desc first code

---
+++ src/test/java/GitClass.java
@@ -0,0 +1,10 @@
+public class GitClass {
+    public static void main(String[] args) {
+        //git commands
+        //git init
+        //git status
+        //git branch -m
+        //git add src/test/java
+        //git
+    }
+}
